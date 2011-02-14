4860d4d5ab3af5071be0d9973c9a81c20cc3d416
Jari Aarniala <jari.aarniala@gmail.com>
2011-02-15 01:10:11 +0200

    Use proper prototype for Vector2D. Seems to bring it down in profiling results.

--- vector.js
+++ vector.js
@@ -1,55 +1,51 @@
-function Point(x, y, cache) {
-	return Vector2D(x, y, cache)
+function Point(x, y) {
+	return new Vector2D(x, y)
 }
 
 // Number -> Number -> Vector2D
-function Vector2D(x, y, cache) {
-	if (cache) {
-		cache.x = x
-		cache.y = y
-		return cache
-	}
-	return {
-		x : x, 
-		y : y,
+function Vector2D(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Vector2D.prototype = {
 		// Vector2D -> Vector2D
-		add : function(other, cache) { return Vector2D(this.x + other.x, this.y + other.y, cache) },
+		add : function(other) { return new Vector2D(this.x + other.x, this.y + other.y) },
 		// Vector2D -> Vector2D
-		subtract : function(other, cache) { return this.add(other.invert(cache), cache) },
+		subtract : function(other) { return this.add(other.invert()) },
 		// Unit -> Number
 		getLength : function() { return Math.sqrt(this.x * this.x + this.y * this.y) },
 		// Number -> Vector2D
-		times : function(multiplier, cache) { return Vector2D(this.x * multiplier, this.y * multiplier, cache) },
+		times : function(multiplier) { return new Vector2D(this.x * multiplier, this.y * multiplier) },
 		// Unit -> Vector2D
-		invert : function(cache) { return Vector2D(-this.x, -this.y, cache) },
+		invert : function() { return new Vector2D(-this.x, -this.y) },
 		// Number -> Vector2D
-		withLength : function(newLength, cache) { return this.times(newLength / this.getLength(), cache) },
-		rotateRad : function(radians, cache) {
+		withLength : function(newLength) { return this.times(newLength / this.getLength()) },
+		rotateRad : function(radians) {
 			var length = this.getLength()			
-			var currentRadians = this.getAngle(cache)
+			var currentRadians = this.getAngle()
 			var resultRadians = radians + currentRadians
-			var rotatedUnit = Vector2D(Math.cos(resultRadians), Math.sin(resultRadians), cache)
-			return rotatedUnit.withLength(length, cache)			
+			var rotatedUnit = new Vector2D(Math.cos(resultRadians), Math.sin(resultRadians))
+			return rotatedUnit.withLength(length)			
 		},
 		// Number -> Vector2D
-		rotateDeg : function(degrees, cache) {
+		rotateDeg : function(degrees) {
 			var radians = degrees * 2 * Math.PI / 360
-			return this.rotateRad(radians, cache)
+			return this.rotateRad(radians)
 		},
 		// Unit -> Number
-		getAngle : function(cache) {
+		getAngle : function() {
 			var length = this.getLength()
-			unit = this.withLength(1, cache)
+			unit = this.withLength(1)
 			return Math.atan2(unit.y, unit.x)			
 		}, 
-		getAngleDeg : function(cache) {
-		  return this.getAngle(cache) * 360 / (2 * Math.PI)
+		getAngleDeg : function() {
+		  return this.getAngle() * 360 / (2 * Math.PI)
 		}, 
-		floor : function(cache) {
-			return Vector2D(Math.floor(this.x), Math.floor(this.y), cache)
+		floor : function() {
+			return new Vector2D(Math.floor(this.x), Math.floor(this.y))
 		},
 		toString : function() {
 			return "(" + x + ", " + y + ")"
 		}
-	}
 }
