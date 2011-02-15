9f48d0d2010bbf086c58151bc3a17a9666a3a3b2
Juha Paananen <juha.paananen@reaktor.fi>
2011-02-15 13:17:37 +0200

    MessageQueue tries to do a better job at cleaning up subscriptions

--- worzone.js
+++ worzone.js
@@ -310,12 +310,39 @@ function Keyboard() {
 	}	
 }
 
-function MessageQueue() {
-    var observers = []
+function MessageQueue() {     
+    function remove(xs, x) {
+       xs.splice(xs.indexOf(x), 1)
+    }      
+    function Subscription(observable) {
+      var disposable              
+      function cancel() { remove(subscriptions, subscription)}                                   
+      function push(message) { messageQueue.push(message) }
+      function start() {
+        disposable = observable.Subscribe( push, cancel)        
+      } 
+      function stop() {
+        if (disposable) disposable.Dispose()  
+      }                   
+      var subscription = {
+        start : start, stop : stop
+      }                 
+      subscriptions.push(subscription)
+      if (observers.length > 0) { start() }
+      return subscription;
+    }                                 
+    var subscriptions = []
+    var observers = []    
     var messageQueue = Rx.Observable.Create(function(observer) {                               
         observers.push(observer)
+        if (observers.length == 1) {
+          subscriptions.forEach(function(subscription) { subscription.start() })
+        }
 		    return function() { 
-		      observers.splice(observers.indexOf(observer), 1)
+		      remove(observers, observer); 
+		      if (observers.length == 0) {
+		        subscriptions.forEach(function(subscription) { subscription.stop() })
+		      }
 		    }
     })    
     messageQueue.ofType = function(messageType) { return messageQueue.Where(function(message) { return message.message == messageType})}
@@ -326,7 +353,7 @@ function MessageQueue() {
         return messageQueue
     }
     messageQueue.plug = function (observable) {
-        observable.Subscribe(function(message) {messageQueue.push(message)})
+        Subscription(observable)
         return messageQueue
     }    
     return messageQueue
